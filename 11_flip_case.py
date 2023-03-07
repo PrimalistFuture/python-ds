17eeb4644ab2903baec91418da334230014c893a
Alyssa Denary <alyssa.denary@gmail.com>
2023-03-06 20:36:28 -0500

    add: flip case

--- 11_flip_case.py
+++ 11_flip_case.py
@@ -11,3 +11,20 @@ def flip_case(phrase, to_swap):
         'AaaaHHH'
 
     """
+    # initialize a variable for flipped_case_phrase
+    # iterate over the phrase
+    #   compare that letter with the uppercase version of the letter
+    #   if they match, lowercase the letter and concat on flipped_case_phrase
+    #   else, uppercase and concat
+    #   return
+
+    flipped_case_phrase = ""
+    for letter in phrase:
+        if letter.lower() == to_swap.lower():
+            if letter == letter.upper():
+                flipped_case_phrase += letter.lower()
+            else:
+                flipped_case_phrase += letter.upper()
+        else:
+            flipped_case_phrase += letter
+    return flipped_case_phrase
