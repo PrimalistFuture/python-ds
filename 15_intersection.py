bc3a492d2f18fbeb335679fbe07779016dbfa40a
Alyssa Denary <alyssa.denary@gmail.com>
2023-03-07 14:59:51 -0500

    add: intersection

--- 15_intersection.py
+++ 15_intersection.py
@@ -1,15 +1,18 @@
 def intersection(l1, l2):
     """Return intersection of two lists as a new list::
-    
+
         >>> intersection([1, 2, 3], [2, 3, 4])
         [2, 3]
-        
+
         >>> intersection([1, 2, 3], [1, 2, 3, 4])
         [1, 2, 3]
-        
+
         >>> intersection([1, 2, 3], [3, 4])
         [3]
-        
+
         >>> intersection([1, 2, 3], [4, 5, 6])
         []
     """
+    # note: quadratic time
+    intersection_list = [item for item in l1 if item in l2]
+    return intersection_list
